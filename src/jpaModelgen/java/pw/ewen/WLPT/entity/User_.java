120f50df2ee4b950719dd35a78e32f4c14f98b70
ewen <ewen80@gmail.com>
2017-02-17 00:56:47 +0800

    增加 meta modal 自生成源码文件

---
+++ src/jpaModelgen/java/pw/ewen/WLPT/entity/User_.java
@@ -0,0 +1,18 @@
+package pw.ewen.WLPT.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(User.class)
+public abstract class User_ {
+
+	public static volatile SingularAttribute<User, String> password;
+	public static volatile SingularAttribute<User, Role> role;
+	public static volatile SingularAttribute<User, String> name;
+	public static volatile SingularAttribute<User, String> ID;
+	public static volatile SingularAttribute<User, String> picture;
+
+}
+
