53c90d8000a6f1b12c1f5d27dda3bcc78f1f0dd5
wenliang <ewen80@qq.com>
2021-02-18 16:57:40 +0800

    ''

--- src/main/java/pw/ewen/WLPT/controllers/AuthenticationApi.java
+++ src/main/java/pw/ewen/WLPT/controllers/AuthenticationApi.java
@@ -3,10 +3,7 @@ package pw.ewen.WLPT.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pw.ewen.WLPT.repositories.UserRepository;
 
 import java.util.Base64;
@@ -15,28 +12,25 @@ import java.util.HashMap;
 /**
  * Created by wenliang on 17-2-13.
  * 用户认证模块
- *
- * TODO:移植逻辑到services层
  */
 @RestController
 @RequestMapping(value = "/authentication")
 public class AuthenticationApi {
 
     @Autowired
-    private UserRepository userRepository;
-    @Autowired
     private UserDetailsService userDetailsService;
 
     //用户认证接口
-    @RequestMapping(method= RequestMethod.PUT, produces="application/json")
-    public boolean checkAuthentication(@RequestBody HashMap<String,String> authInfo){
+    @RequestMapping(method= RequestMethod.GET, produces="application/json")
+    public boolean authenticate(@RequestParam(value = "userid", defaultValue = "") String userid,
+                                @RequestParam(value = "token", defaultValue = "") String authToken){
         UserDetails userDetails;
         try{
-            userDetails = userDetailsService.loadUserByUsername(authInfo.get("userId"));
+            userDetails = userDetailsService.loadUserByUsername(userid);
             String authString = userDetails.getUsername() + ":" + userDetails.getPassword();
             byte[] encodedBytes = Base64.getEncoder().encode(authString.getBytes());
             String encodedAuthString = new String(encodedBytes);
-            return authInfo.get("authToken").equals(encodedAuthString);
+            return authToken.equals(encodedAuthString);
 
         }catch (Exception exception){
             return false;
